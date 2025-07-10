834343c8631a5e3e100b54f2f24367be320be098
Bedoel <soiyah1969@gmail.com>
2025-07-11 00:07:45 +0700

    paksa https2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('id');
-        if (env('APP_ENV') === 'production') {
+        if (env('APP_ENV')  != 'local') {
             URL::forceScheme('https');
         }
     }
