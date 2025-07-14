768c0bd8f9e397d387e463d2a1065cea3964ace1
Bedoel <soiyah1969@gmail.com>
2025-07-15 02:11:21 +0700

    tambah .sh

--- run-schedule.sh
+++ run-schedule.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-php artisan schedule:run
-exit 0
+# Make sure this file has executable permissions, run `chmod +x run-cron.sh`
+
+# This block of code runs the Laravel scheduler every minute
+while [ true ]
+    do
+        echo "Running the scheduler..."
+        php artisan schedule:run --verbose --no-interaction &
+        sleep 60
+    done
